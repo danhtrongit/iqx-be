8980de8840ca64c45ce3a33381f59d8c40f104ef
Danh Trọng Trần <danhtrongtran@192.168.1.36>
2025-07-23 16:36:18 +0700

    Cập nhật - 2025-07-23 16:36:18

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,4 @@
 from app.models.daily_prices import DailyPrice
 from app.models.securities import Securities
-from app.models.sensor_data import SensorData
 
 __all__ = ["SensorData", "Securities", "DailyPrice"] 
